222b350a01b62d0aa914b6941a573302df25cb22
sweih <sweih@2d0eed8c-cf10-0410-9de0-ecd75c1cbafd>
2006-07-16 10:02:16 +0000

    Adjust Dropdownmenu

--- nx5/cms/designs/menu/dropdownmenu/dc_dropdownmenu.php
+++ nx5/cms/designs/menu/dropdownmenu/dc_dropdownmenu.php
@@ -26,7 +26,7 @@
 	class DropdownMenu extends AbstractDesign {
 		
 		var $pathToRoot;
-		
+		var $themeName;
 		/**
 		 * Returns the name and the description of the DesignClass for Backoffice adjustments.
 		 *
@@ -45,11 +45,13 @@
 		}
 
 	/**
-  	 * Draw the tabbar.
+  	 * Draw the menu.
   	 */
   	function draw() {
+  	  
   	  // get the path of menues, e.g. if a thrid-level page is active you 
   	  // get the corresponding3rd/2nd/1st level menues
+  	  $this->themeName = 'NX';
   	  
   	  $this->pathToRoot = $this->cds->menu->getPathToRoot();
   	  // get the actice toplevelmenu
@@ -60,12 +62,20 @@
 
   	  $topMenu = array_pop($this->pathToRoot);
       if ($topMenu == null) $topMenu = $startMenu;
-
-  	  $out = '<div id="menuleft">&nbsp;</div><div id="menu">';
+	  $out = "
+	  <SCRIPT LANGUAGE='JavaScript'>
+		var dsMenu =
+		[";
+    
+ 	 
   	  for ($i=0; $i<count($firstLevelMenues); $i++) {
-  	  	$out.='<ul>'.$this->drawMenu($firstLevelMenues[$i]).'</ul>';
+			$out.=$this->drawMenu($firstLevelMenues[$i]); 	
+			if (($i+1) < count($firstLevelMenues))  	  	
+			  $out.=',';
   	  }
-  	  $out.='</div><div id="menuright">&nbsp;</div>';
+  	  $out.=']; </SCRIPT><DIV ID="dsMenu"></DIV>
+
+	<SCRIPT LANGUAGE="JavaScript">cmDraw ("dsMenu", dsMenu, "hbr", cmTheme'.$this->themeName.', "Theme'.$this->themeName.'");</SCRIPT>';
   	  return $out;
   	}
   	
@@ -79,36 +89,25 @@
     $title = $menuObject->getTitle();
  	$link  = $menuObject->getLink();
  	$isPopup = $menuObject->isPopup();
-				 				
-  	$out = '<li>';
-    $out.= '<a '.$add.'href="'.$link.'" ';
- 	if ($isPopup) {
- 	  $out.= ' target="_blank"'; 				  
- 	} 				
- 	$out.='>'.$title.'</a>'; 		
-    $lowerLevel = $menuObject->lowerLevel();
-    if (count($lowerLevel) > 0) {
-      $out.="\n".'<ul>';
-      for ($i=0; $i<count($lowerLevel); $i++) {
-        if ($lowerLevel[$i]->hasLowerLevel()) {      	  
-        	$out.="\n".$this->drawMenu($lowerLevel[$i])."\n";    	
-        } else {
-          
-          $title = $lowerLevel[$i]->getTitle();
- 	      $link  = $lowerLevel[$i]->getLink();
- 	      $isPopup = $lowerLevel[$i]->isPopup();
-				 				
-  		  $out.= '<li>';
-    	  $out.= '<a href="'.$link.'" ';
- 		  if ($isPopup) {
- 	  			$out.= ' target="_blank"'; 				  
- 		  } 				
- 		  $out.='>'.$title.'</a></li>'."\n"; 		        	
-        }
-      }
-      $out.='</ul>';
-    }     
-    $out.= "\n".'</li>'; 	
+	
+ 	if ($isPopup)  {
+		$target = "'_blank'";
+	} else {
+		$target="null";
+	}
+  	$out = "['', '$title', '$link', $target, ''";
+  	if ($menuObject->hasLowerLevel()) {
+  	   $out.=',';
+  		$lowerLevels = $menuObject->lowerLevel();
+  	   for ($i=0; $i<count($lowerLevels); $i++)	{  	   	
+  	   	 $out.=$this->drawMenu($lowerLevels[$i]); 	
+		 if (($i+1) < count($lowerLevels))  	  	
+		   $out.=',';
+  	   }
+  	}
+  	
+  	$out.= "]";
+	
     return $out;						   	
    }
   	
@@ -119,128 +118,12 @@
   	 */
   	function setupPage($layout)	{ 
   		global $cds; 						
-  		$out = '
-  		<style type="text/css"> 
-#menu {
-  padding-top:12px;
-  padding-bottom: 22px;
-  padding-left:5px;
-  background-image:url(' .$cds->docroot. 'sys/designs/menu/dropdownmenu/menubg.gif);
-  background-repeat:repeat-x;
-  float: left;
-  cursor:pointer;  
-}
-#menuleft {
- padding:0px;
- margin:0px;
- background-image:url(' .$cds->docroot. 'sys/designs/menu/dropdownmenu/menubgleft.gif);
- float:left;
- background-repeat:no-repeat;
- width:20px;
- height:50px;
-}
-
-#menuright {
- padding:0px;
- margin:0px;
- background-image:url(' .$cds->docroot. 'sys/designs/menu/dropdownmenu/menubgright.gif);
- float:left;
- background-repeat:no-repeat;
- width:20px;
- height:50px;
-}
-
-#menu ul {
-  list-style: none;
-  margin: 0;
-  padding:2px;
-  width: 130px;
-  float: left;
-  z-index:500;
-}
-
-#menu a, #menu h2 {
-  font: 12px arial, helvetica, sans-serif;
-  color: #999999
-  display: block;
-  border-width: 0px;
-  border-style: solid;
-  margin: 0;
-  padding: 2px 3px;
-}
-
-#menu h2 {
-  color: #999999;
-}
-
-#menu a {
-  color: #999999;
-  text-decoration: none;
-}
-
-#menu a:hover {
-  color: #990000;
-}
-
-#menu li {position: relative;}
-
-#menu ul ul {
-  width:125px;
-  background-color:#ffffff;
-  color:#666666;
-  border:1px #cccccc solid;
-  position: relative;
-  z-index: 500;
-}
-
-#menu ul ul a:hover {
-  color:#990000;
-}
-
-#menu ul ul a {
-  background-color:#ffffff;
-  color:#999999;
-  text-align:left;
-}
-
-
-#menu ul ul ul {
-position: absolute;
-top: 0;
-left: 100%;
-}
-
-div#menu ul ul,
-div#menu ul li:hover ul ul,
-div#menu ul ul li:hover ul ul
-{display: none;}
-
-div#menu ul li:hover ul,
-div#menu ul ul li:hover ul,
-div#menu ul ul ul li:hover ul
-{display: block;}
-
-</style>
-<!--[if IE]>
-<style type="text/css" media="screen">
- #menu ul li {float: left; width: 100%;}
-</style>
-<![endif]-->
-<!--[if lt IE 7]>
-<style type="text/css" media="screen">
-body {
-  behavior: url(' .$cds->docroot. 'sys/designs/menu/dropdownmenu/csshover.htc);
-}
-
-#menu ul li a {height: 1%;} 
-
-#menu a, #menu h2 {
-font: 12px arial, helvetica, sans-serif;
-}
-</style>
-<![endif]-->';
-	
-			$layout->addToHeader($out);
+  		$this->themeName = 'NX';
+  		$out = '<SCRIPT type="text/javascript" LANGUAGE="JavaScript" SRC="'.$cds->docroot.'sys/designs/menu/dropdownmenu/JSCookMenu.js"></SCRIPT>
+<LINK REL="stylesheet" HREF="'.$cds->docroot.'sys/designs/menu/dropdownmenu/Theme'.$this->themeName.'/theme.css" TYPE="text/css">
+<SCRIPT LANGUAGE="JavaScript" type="text/javascript" SRC="'.$cds->docroot.'sys/designs/menu/dropdownmenu/Theme'.$this->themeName.'/theme.js"></SCRIPT>  		
+  		';
+  		$layout->addToHeader($out);
   	}
   	
 	}
