1fbbc8b6a2d40579d49dce687ae3df9125b6a1ce
sweih <sweih@2d0eed8c-cf10-0410-9de0-ecd75c1cbafd>
2007-01-05 14:32:16 +0000

    Add Google Keys

--- nx5/cms/config.inc.php
+++ nx5/cms/config.inc.php
@@ -25,9 +25,9 @@
 
 	// Database variables.
 	$c["dbhost"] = "localhost";  				// name of the mysql-Database. Standard-port is used.
-	$c["database"] = "hanmee";   					// name of the database. 
+	$c["database"] = "demo";   					// name of the database. 
 	$c["dbuser"] = "root";       				// name of the database user.
-	$c["dbpasswd"] = "";         			// password of the database user.
+	$c["dbpasswd"] = "test";         			// password of the database user.
 	$c["dbdriver"] = "mysql";    				// type of your database. Do not change.
 	$c["dbnow"] = "NOW()";       				// for future versions. Do not change.
 	$c["dbcode"] = "3dYqpm8UhF"; 				// for future versions. Do not change.
@@ -51,7 +51,7 @@
 	$c["renderstatichtml"] = false;              // enable Rendering of Static HTML-Code (Caching)
 
 	// Dynamic Cache
-	$JPCACHE_ON = true;    					// Turn dynamic page caching on/off
+	$JPCACHE_ON = false;    					// Turn dynamic page caching on/off
 	$JPCACHE_TIME = 3600;   						// Default number of seconds to cache a page
 	$JPCACHE_USE_GZIP = 0; 						// Whether or not to use GZIP for transmission of the page to a browser
 
@@ -60,8 +60,12 @@
 	$QUERY_CACHE_TIME = 900; 					// Default number of seconds to cache a query
 
 	
+	// Get GOOGLE Ready
+	$c['googlemapskey'] = '';
+	$c['googleanalyticskey'] = '';
+		
+	
 	// CDS Configuration
-
 	
   	// statistics
   	$c["pagetracking"] = true;                   // enable page-statistics
@@ -110,8 +114,8 @@
 	// base paths
 	// attention: please read the readme file to know, which paths must be set writable! 
 	// (all www and wwwdev + subfolders)
-	$c["basepath"] = "C:\\Web\\hanmee/";				// path of your nx-home-folder on harddrive. ends with slash.
-	$c["basedocroot"] = "/hanmee/";		// docroot on your webserver, which corresponds 
+	$c["basepath"] = "C:\\Web\\nx5/";				// path of your nx-home-folder on harddrive. ends with slash.
+	$c["basedocroot"] = "/nx5/";		// docroot on your webserver, which corresponds 
 											// to the path you just entered. starts and ends with a slash!
 	$c["host"] = "http://localhost"; 				// address of your web-server. Ends without slash
 	$c["cmsfolder"] = "cms";					// name or relative path of the folder cms, if renamed.
